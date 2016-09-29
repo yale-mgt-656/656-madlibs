d49c57d9031f9a002c463a70261aa806c56a1501
Aimee Shahir <aimee.shahir@yale.edu>
2016-09-29 19:50:00 +0000

    added verbs

---
+++ pos/verbs/aimeeclaudi.js
@@ -0,0 +1,16 @@
+
+module.exports.verbs = [
+    'swim',
+    'shout',
+    'dance',
+    'sing',
+    'fall',
+    'eat',
+    'skate',
+    'open',
+    'watch',
+    'commit',
+    'initiate',
+    'present',
+    'drink',
+];
