6aad843327c7046d89beb8d249ad273610c89cbd
Colin MacDougall <colin.macdougall@yale.edu>
2016-09-29 19:48:59 +0000

    Get some orang utans to Svalbard

---
+++ pos/nouns/our-new-nouns.js
@@ -0,0 +1,16 @@
+module.exports.nouns = [
+  'large hadron collider',
+  'human',
+  'rolls',
+  'candyman',
+  'Ted',
+  'fork',
+  'orang utan',
+  'Scylla',
+  'Charybdis',
+  'papyrus',
+  'Elon Musk',
+  'Tanzania',
+  'Svalbard',
+  'Harry Potter'
+  ];
